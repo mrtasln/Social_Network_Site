9e17cf22c1b1de26af3b4caed9a965eae23c5480
ersin9226 <ersin_9226@hotmail.com>
2013-11-26 13:15:10 +0200

    Ana Sayfa

---
+++ deneme.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<title>Sosyal Paylasim Sitesi</title>
+</head>
+<body>
+<div class="headerMenu">
+ <div id="wrapper">
+   <div class="logo">
+     <img src="img/Z.png">
+   </div>
+   <div class="search_box">
+        <form method="GET" action="search.php" id="search">
+        <input name="q" type="text" size="60" placeholder="search . . ." />
+        </form>
+    </div>
+     
+    <div id="menu">
+      <a href="#">AnaSayfa</a>
+      <a href="#">Hakkinda</a>
+      <a href="#">Kayit Ol</a>
+      <a href="#">Giris Yap</a>
+    </div>
+</div>
+</div>
+<br />
+<br />
+<br />
+<br />
+</body>
+</html>
