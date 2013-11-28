eefbd1864ba6d86087a5d25b174dedad73265298
CagriKaraaslan <mehendis1903@hotmail.com>
2013-11-28 12:56:41 +0200

    Giriş Sayfası Başlangıç

---
+++ GirisSayfasi.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<title>Sosyal Paylasim Sitesi</title>
+</head>
+<body>
+<div class="headerMenu">
+ <div id="wrapper">
+   <div class="logo">
+     <img src="img/Z.png">
+   </div>
+   <div class="search_box">
+        <form method="GET" action="search.php" id="search">
+        <input name="q" type="text" size="60" placeholder="search . . ." />
+        </form>
+    </div>
+     
+    <div id="menu">
+      <a href="index1.php">AnaSayfa</a>
+      <a href="hakkimizda.php">Hakkinda</a>
+      <a href="KayitOl2.php">Kayit Ol</a>
+      <a href="GirisSayfasi.php">Giris Yap</a>
+    </div>
+</div>
+</div>
+<br />
+<br />
+<br />
+<br />
+<table width="100%" class="homepageTable">
+        <tr>
+             <td width="55%" valign="top">
+             <h2>Hemen Bize Katılın    </h2>
+             <img src="img/indir.jpg" width="500" /></td>
+             <td width="45%" valign="top">
+           <p>&nbsp;</p>
+           <p>&nbsp;</p>
+           <p>&nbsp;</p>
+           <p>&nbsp;</p>
+           <p>&nbsp;</p></td>
+        </tr>
+        <tr>
+          <td valign="top">&nbsp;</td>
+          <td valign="top">&nbsp;</td>
+        </tr>
+</table>
+</body>
+</html> 
