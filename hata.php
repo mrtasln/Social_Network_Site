258fad2847cc3947f6d83afa39e919dbd3274b7e
CagriKaraaslan <mehendis1903@hotmail.com>
2013-11-30 16:48:26 +0200

    Hata Sayfası

---
+++ hata.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<title>Sosyal Paylasim Sitesi</title>
+</head>
+<body>
+<div class="headerMenu">
+ <div id="wrapper">
+   <div class="logo">
+     <img src="img/Z.png">
+   </div>
+   <div class="search_box">
+        <form method="GET" action="search.php" id="search">
+        <input name="q" type="text" size="60" placeholder="search . . ." />
+        </form>
+    </div>
+     
+    <div id="menu">
+      <a href="index1.php">AnaSayfa</a>
+      <a href="#">Hakkinda</a>
+      <a href="KayitOl2.php">Kayit Ol</a>
+      <a href="GirisSayfasi.php">Giris Yap</a>
+    </div>
+</div>
+</div>
+<p><br />
+  <br />
+</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><br />
+  <br />
+</p>
+<table width="100%" class="homepageTable">
+        <tr>
+          <td width="1%" valign="top">&nbsp;</td>
+             <td width="99%" valign="top">
+           <h1 id="x">Lütfen Kullanıcı Adınızı ve Sifrenizi </h1>
+           <h1 id=x><a href="GirisSayfasi.php">
+           tekrar giriniz</a>             &nbsp;</h1>
+           </td>
+        </tr>
+        <tr>
+          <td valign="top">&nbsp;</td>
+          <td valign="top">&nbsp;</td>
+        </tr>
+</table>
+</body>
+</html> 
