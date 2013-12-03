a223928623f049e5305ff64db2ee922362329318
CagriKaraaslan <mehendis1903@hotmail.com>
2013-12-03 12:20:55 +0200

    Banner Giriş

---
+++ deneme1.php
@@ -0,0 +1,59 @@
+<?php
+//initialize the session
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+// ** Logout the current user. **
+$logoutAction = $_SERVER['PHP_SELF']."?doLogout=true";
+if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")){
+  $logoutAction .="&". htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
+  //to fully log out a visitor we need to clear the session varialbles
+  $_SESSION['MM_Username'] = NULL;
+  $_SESSION['MM_UserGroup'] = NULL;
+  $_SESSION['PrevUrl'] = NULL;
+  unset($_SESSION['MM_Username']);
+  unset($_SESSION['MM_UserGroup']);
+  unset($_SESSION['PrevUrl']);
+	
+  $logoutGoTo = "index1.php";
+  if ($logoutGoTo) {
+    header("Location: $logoutGoTo");
+    exit;
+  }
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<title>Sosyal Paylasim Sitesi</title>
+</head>
+<body>
+<div class="headerMenu">
+ <div id="wrapper">
+   <div class="logo">
+     <img src="img/Z.png">
+   </div>
+   <div class="search_box">
+        <form method="GET" action="search.php" id="search">
+        <input name="q" type="text" size="60" placeholder="search . . ." />
+        </form>
+    </div>
+     
+    <div id="menu">
+      <a href="deneme1.php">AnaSayfa</a>
+      <a href="profile.php?u=<?php  echo $_SESSION['MM_Username'] ?>"><?php  echo $_SESSION['MM_Username'] ?>'in Profili</a>
+      <a href="hesap_secenekleri.php">Hesap Secenekleri</a>
+      <a href="<?php echo $logoutAction ?>">Cikis Yap</a>
+    </div>
+</div>
+</div>
+<br />
+<br />
+<br />
+<br />
