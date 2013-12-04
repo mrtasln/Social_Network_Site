39583eef96d518dc0c40192708bf745b0a169890
CagriKaraaslan <mehendis1903@hotmail.com>
2013-12-04 20:38:46 +0200

    Arkadaş Ekleme devam

--- friend_requests.php
+++ friend_requests.php
@@ -27,3 +27,44 @@ else
 	}
 }
 ?>
+<?php
+if(isset($_POST['acceptrequest'.$user_from])) {
+	$get_friend_check = mysql_query("SELECT arkadas_dizisi FROM uyeler WHERE kullanici_adi='$kullanici'");
+	$get_friend_row = mysql_fetch_assoc($get_friend_check);
+	$friend_array = $get_friend_row['arkadas_dizisi'];
+	$friendArray_explode = explode(",",$friend_array);
+	$friendArray_count = count('$friendArray_explode');
+	
+	$get_friend_check_friend = mysql_query("SELECT arkadas_dizisi FROM uyeler WHERE kullanici_adi='$user_from'");
+	$get_friend_row_friend = mysql_fetch_assoc($get_friend_check_friend);
+	$friend_array_friend = $get_friend_row_friend['arkadas_dizisi'];
+	$friendArray_explode_friend = explode(",",$friend_array_friend);
+	$friendArray_count_friend = count($friendArray_explode_friend);
+	
+	if($friend_array == "") {
+		$friendArray_count = count(NULL);
+	}
+	if($friend_array_friend == "") {
+		$friendArray_count_friend = count(NULL);
+	}
+	if($friendArray_count == NULL) {
+		$add_friend_query = mysql_query("UPDATE uyeler SET arkadas_dizisi=CONCAT(arkadas_dizisi,'$user_from') WHERE kullanici_adi='$kullanici'");
+	}
+	if($friendArray_count_friend == NULL) {
+		$add_friend_query = mysql_query("UPDATE uyeler SET arkadas_dizisi=CONCAT(arkadas_dizisi,'$user_to') WHERE kullanici_adi='$user_from'");
+	}
+	if($friendArray_count_friend >= 1) {
+		$add_friend_query = mysql_query("UPDATE uyeler SET arkadas_dizisi=CONCAT(arkadas_dizisi,',$user_from') WHERE kullanici_adi='$kullanici'");
+	}
+	if($friendArray_count_friend >= 1) {
+	$add_friend_query = mysql_query("UPDATE uyeler SET arkadas_dizisi=CONCAT(arkadas_dizisi,',$user_to') WHERE kullanici_adi='$user_from'");
+	}
+	echo "Sen simdi arkadasisin";
+	
+}
+?>
+<form action="friend_requests.php" method="post">
+<input type="submit" name="acceptrequest<?php echo $user_from; ?>" value="Istegi KabulEt" />
+<input type="submit" name="ignorerequest<?php echo $user_from; ?>" value="Istegi Reddet" 
+/>
+</form>
