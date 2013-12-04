16dc8443548832c84d9f9a854105726025ec725e
ersin9226 <ersin_9226@hotmail.com>
2013-12-04 14:39:37 +0200

    Profil Sayfasına Eklemeler

--- profile.php
+++ profile.php
@@ -1,6 +1,61 @@
 <?php include("deneme1.php") ?>
 <?php require_once('Connections/varitabanibaglantim.php'); ?>
 
+<?php
+if(isset($_SESSION['MM_Username']))
+{
+	$kullanici = $_SESSION['MM_Username'];
+}
+else 
+{
+	$kullanici="";
+}
+
+if(isset($_GET['u']))
+{
+	$kullanici_adi=mysql_real_escape_string($_GET['u']);
+	if(ctype_alnum($kullanici_adi)){
+	$check=mysql_query("SELECT kullanici_adi,isim FROM uyeler WHERE kullanici_adi='$kullanici_adi'");
+	if(mysql_num_rows($check)==1)
+	{
+		$get=mysql_fetch_assoc($check);
+		$kullanici_adi=$get['kullanici_adi'];
+		$isim=$get['isim'];
+	}
+	else
+	{
+		echo "<meta http-equiv=\"refresh\" content=\"0; url=http://localhost:81/SocialNet/index.php\">";
+	    exit();
+	}
+	}
+}
+
+/*$post = @$_POST['post'];
+	if($post != "") 
+	{
+		$date_added = date("Y-m-d");
+		$added_by= $kullanici_adi;
+		$user_posted_to = "test123";
+		
+		$sqlCommand = "INSERT INTO posts VALUES('','$post','$date_added','$added_by','$user_posted_to')";
+		$query=mysql_query($sqlCommand) or die (mysql_error());
+	*/
+
+	
+	$check_pic=mysql_query("SELECT profil_resmi FROM uyeler WHERE kullanici_adi='$kullanici_adi'");
+	$get_pic_row=mysql_fetch_assoc($check_pic);
+	$profile_pic_db=$get_pic_row['profil_resmi'];
+	if($profile_pic_db == "")
+	{
+		$profil_pic="img/images1.jpg";
+	}
+	else
+	{
+	$profil_pic="userdata/profile_pics/".$profile_pic_db;	
+	}
+	
+?>
+
 <div id="status"></div>
 <div class="postForm">
 <form action="profile.php?u=<?php  echo $_SESSION['MM_Username'] ?>" method="post">
