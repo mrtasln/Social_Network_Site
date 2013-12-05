2b85c2e2bb834af92fc78edd2a21719d9685a38d
mrtasln <murat_1992_51@outlook.com>
2013-12-05 11:17:51 +0200

    Arkadaş ekleme son hali

--- friend_requests.php
+++ friend_requests.php
@@ -15,6 +15,7 @@ $friendRequests = mysql_query("SELECT * FROM friend_requests WHERE user_to='$kul
 $numrows = mysql_num_rows($friendRequests);
 if($numrows == 0) {
 	echo "Arkadaslik Istegin Yok";
+	$user_from = "";
 }
 else
 {
@@ -23,10 +24,11 @@ else
 		$user_to = $get_row['user_to'];
 		$user_from = $get_row['user_from'];
 		
-		echo '' . $user_from . ' arkadas olmak istiyor';
-	}
-}
+		echo '' . $user_from . ' arkadas olmak istiyor'.'<br />';
+		
+
 ?>
+
 <?php
 if(isset($_POST['acceptrequest'.$user_from])) {
 	$get_friend_check = mysql_query("SELECT arkadas_dizisi FROM uyeler WHERE kullanici_adi='$kullanici'");
@@ -35,36 +37,49 @@ if(isset($_POST['acceptrequest'.$user_from])) {
 	$friendArray_explode = explode(",",$friend_array);
 	$friendArray_count = count('$friendArray_explode');
 	
-	$get_friend_check_friend = mysql_query("SELECT arkadas_dizisi FROM uyeler WHERE kullanici_adi='$user_from'");
-	$get_friend_row_friend = mysql_fetch_assoc($get_friend_check_friend);
-	$friend_array_friend = $get_friend_row_friend['arkadas_dizisi'];
-	$friendArray_explode_friend = explode(",",$friend_array_friend);
-	$friendArray_count_friend = count($friendArray_explode_friend);
+	$add_friend_check_username = mysql_query("SELECT arkadas_dizisi FROM uyeler WHERE kullanici_adi='$user_from'");
+	$get_friend_row_username = mysql_fetch_assoc($add_friend_check_username);
+	$friend_array_username = $get_friend_row_username['arkadas_dizisi'];
+	$friendArray_explode_username = explode(",",$friend_array_username);
+	$friendArray_count_username = count($friendArray_explode_username);
+	echo $friendArray_count_username." Number for friend ($user_from)<br />";
+	echo $friendArray_count." Number for logged in User ($kullanici)<br />";
 	
 	if($friend_array == "") {
 		$friendArray_count = count(NULL);
 	}
-	if($friend_array_friend == "") {
+	if($friend_array_username == "") {
 		$friendArray_count_friend = count(NULL);
 	}
 	if($friendArray_count == NULL) {
 		$add_friend_query = mysql_query("UPDATE uyeler SET arkadas_dizisi=CONCAT(arkadas_dizisi,'$user_from') WHERE kullanici_adi='$kullanici'");
 	}
-	if($friendArray_count_friend == NULL) {
+	if($friendArray_count_username == NULL) {
 		$add_friend_query = mysql_query("UPDATE uyeler SET arkadas_dizisi=CONCAT(arkadas_dizisi,'$user_to') WHERE kullanici_adi='$user_from'");
 	}
-	if($friendArray_count_friend >= 1) {
+	if($friendArray_count >= 1) {
 		$add_friend_query = mysql_query("UPDATE uyeler SET arkadas_dizisi=CONCAT(arkadas_dizisi,',$user_from') WHERE kullanici_adi='$kullanici'");
 	}
-	if($friendArray_count_friend >= 1) {
+	if($friendArray_count_username >= 1) {
 	$add_friend_query = mysql_query("UPDATE uyeler SET arkadas_dizisi=CONCAT(arkadas_dizisi,',$user_to') WHERE kullanici_adi='$user_from'");
 	}
-	echo "Sen simdi arkadasisin";
-	
+	$delete_requests = mysql_query("DELETE FROM friend_requests WHERE user_to='$user_to'&&user_from='$user_from'");
+	echo "Arkadaslik Istegi Kabul Edildi";
+	header("Location: friend_requests.php");
+
+}
+if(isset($_POST['ignorerequest'.$user_from])) {
+	$delete_requests = mysql_query("DELETE FROM friend_requests WHERE user_to='$user_to'&&user_from='$user_from'");
+	echo "Arkadaslik Istegi reddedildi";
+	header("Location: friend_requests.php");
 }
 ?>
+
 <form action="friend_requests.php" method="post">
 <input type="submit" name="acceptrequest<?php echo $user_from; ?>" value="Istegi KabulEt" />
-<input type="submit" name="ignorerequest<?php echo $user_from; ?>" value="Istegi Reddet" 
-/>
+<input type="submit" name="ignorerequest<?php echo $user_from; ?>" value="Istegi Reddet" />
 </form>
+<?php 
+	}
+}
+?>
