e10783fbbdb78048f1c1e0327b3d2def9940880e
CagriKaraaslan <mehendis1903@hotmail.com>
2013-12-14 21:28:55 +0200

    mesajlar sayfası

--- mesajlarim.php
+++ mesajlarim.php
@@ -88,5 +88,63 @@ else
 {
 	$kullanici="";
 }
+
+$grab_messages=mysql_query("SELECT * FROM pvt_messages WHERE user_to='$kullanici' && opened='evet' && deleted='hayir'");
+$numrows_read = mysql_numrows($grab_messages);
+if($numrows_read != 0) {
+while($get_msg=mysql_fetch_assoc($grab_messages)) {
+	$id = $get_msg['id'];
+	$user_from = $get_msg['user_from'];
+	$user_to = $get_msg['user_to'];
+	$msg_title = $get_msg['msg_title'];
+	$msg_body = $get_msg['msg_body'];
+	$date = $get_msg['date'];
+	$opened = $get_msg['opened'];
+	$deleted = $get_msg['deleted'];
+	?>
+    <script language="javascript">
+function toggle<?php echo $id; ?>() {
+	var ele = document.getElementById("toggleText<?php echo $id; ?>");
+	var text = document.getElementById("displayText<?php echo $id; ?>");
+	if(ele.style.display == "block") {
+		ele.style.display = "none";
+	}
+	else
+	{
+		ele.style.display = "block";
+	}
+}
+
+</script>
+    <?php
+	
+	if(strlen($msg_body)> 150) {
+	$msg_body = substr($msg_body, 0, 150)." ....";
+	}
+	else
+	$msg_body = $msg_body;
+	
+	if(@$_POST['delete_' . $id . '']) {
+	$delete_msg_query = mysql_query("UPDATE pvt_messages SET deleted='evet' WHERE id='$id'");
+	}
+	
+	echo "
+	<form method='post' action='mesajlarim.php' name='$msg_title'>
+	<b><a href='profile.php?u=$user_from'>$user_from</a></b>
+	<input type='button' name='openmsg' value='$msg_title' onclick='javascript:toggle$id()'>
+	<input type='submit' name='delete_$id' value=\"Sil\" title='Mesaji Sil'>
+	<input type='submit' name='reply_$id' value='Cevapla'>
+	</form>
+<div id='toggleText$id' style='display: none;'>
+<br />$msg_body
+</div>
+<hr /><br />
+	";
+}
+}
+else
+{
+echo "Sen Okumak İcin Bir Mesaj a Sahip Degilsin";	
+}
 ?>
 	
